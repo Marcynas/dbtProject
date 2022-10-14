b842b9431906c08227575e1c32410268d1b40746
Martynas Tvaska <73237433+Marcynas@users.noreply.github.com>
2022-10-14 15:48:56 +0300

    datesIncremental model:incremental

--- models/example/datesIncremental.sql
+++ models/example/datesIncremental.sql
@@ -1,8 +1,11 @@
 {{ config(materialized = 'incremental') }}
 
-SELECT *
-FROM "SNOWFLAKE_SAMPLE_DATA"."TPCDS_SF10TCL"."DATE_DIM"
-WHERE d_date <= current_date
+SELECT
+    *
+FROM
+    "SNOWFLAKE_SAMPLE_DATA"."TPCDS_SF10TCL"."DATE_DIM"
+WHERE
+    d_date <= current_date
 
 {% if is_incremental() %}
     and d_date > (selext max(d_date) from {{ this }})
