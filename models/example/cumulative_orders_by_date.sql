02fe7d1d7e8997729b7498a9af458453cd77c63b
Martynas Tvaska <73237433+Marcynas@users.noreply.github.com>
2022-10-14 15:15:17 +0300

    cumulative_orders_by_date

---
+++ models/example/cumulative_orders_by_date.sql
@@ -0,0 +1,11 @@
+{{ config(materialized = 'table') }}
+
+SELECT
+    o_orderdate AS date,
+    o_totalprice AS price,
+    SUM(o_totalprice) OVER (
+        ORDER BY
+            o_orderdate
+    ) AS cumulativesum
+FROM
+    "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS"
