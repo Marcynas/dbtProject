f618ceb05011a63cedfc69dcc0a131f081644766
Martynas Tvaska <73237433+Marcynas@users.noreply.github.com>
2022-10-19 13:46:39 +0300

    styling

--- models/example/snowflake_customer_purchases.sql
+++ models/example/snowflake_customer_purchases.sql
@@ -1,9 +1,24 @@
+
+
+WITH orders AS (
+    SELECT *
+    FROM {{ source('sample','orders') }}
+),
+
+customer AS (
+    SELECT *
+    FROM {{ source('sample','customer') }}
+)
+
+
+
 SELECT
     c.c_custkey,
     c.c_name,
     c.c_nationkey AS nation,
     sum(o.o_totalprice) AS total_order_price
 FROM
-    {{ source('sample','customer') }} c
-    left join {{ source('sample','orders') }} o ON c.c_custkey = o.o_custkey
+    customer c
+    LEFT JOIN orders o ON c.c_custkey = o.o_custkey
 {{group_by(3)}}
+
